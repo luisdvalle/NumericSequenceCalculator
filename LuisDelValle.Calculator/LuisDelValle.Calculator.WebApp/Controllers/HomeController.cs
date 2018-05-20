1dc48aac75ef8c05121df3faebb8da51eddf7a56
luisdvalle <jums.delvalle@gmail.com>
2018-05-20 21:47:14 +1000

    Adding razor logic in Index view for home page as well as form to get input number

--- LuisDelValle.Calculator/LuisDelValle.Calculator.WebApp/Controllers/HomeController.cs
+++ LuisDelValle.Calculator/LuisDelValle.Calculator.WebApp/Controllers/HomeController.cs
@@ -1,16 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using LuisDelValle.Calculator.CalculatorService.Models;
+using LuisDelValle.Calculator.WebApp.Services;
+using LuisDelValle.Calculator.WebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LuisDelValle.Calculator.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private IRestClient _restClient;
+
+        public HomeController(IRestClient restClient)
+        {
+            _restClient = restClient;
+            _restClient.Host = "http://localhost:5001/";
+            _restClient.Path = "api/sequences?number=";
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            SequencesViewModel vm = new SequencesViewModel();
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SequencesViewModel vm)
+        {
+            if (vm.InputNumber < 1)
+            {
+                ModelState.AddModelError("", "Output number can't be less than 1");
+
+                return View(vm);
+            }
+
+            if (ModelState.IsValid)
+            {
+                _restClient.Path = _restClient.Path + vm.InputNumber;
+                SequencesResponse sequences = await _restClient.GetResponseAsync() as SequencesResponse;
+            }
+
+            return View(vm);
         }
     }
 }
