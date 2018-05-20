c313d2d29c997c4d82b9ddaf0e5a49d2cfbf3cdf
luisdvalle <jums.delvalle@gmail.com>
2018-05-20 21:45:49 +1000

    Registering RestClient in DI and adding reference to view model in Imports file

--- LuisDelValle.Calculator/LuisDelValle.Calculator.WebApp/Startup.cs
+++ LuisDelValle.Calculator/LuisDelValle.Calculator.WebApp/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LuisDelValle.Calculator.WebApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,8 @@ namespace LuisDelValle.Calculator.WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddScoped<IRestClient, RestClient>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
