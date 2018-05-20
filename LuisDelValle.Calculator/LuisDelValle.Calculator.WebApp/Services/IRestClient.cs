aef13545801e87254dd1d8a059f8d2d2cf566fc1
luisdvalle <jums.delvalle@gmail.com>
2018-05-20 21:31:28 +1000

    Implementing class to consume WebApi

---
+++ LuisDelValle.Calculator/LuisDelValle.Calculator.WebApp/Services/IRestClient.cs
@@ -0,0 +1,13 @@
+﻿using LuisDelValle.Calculator.Abstractions;
+using System.Threading.Tasks;
+
+namespace LuisDelValle.Calculator.WebApp.Services
+{
+    public interface IRestClient
+    {
+        string Host { get; set; }
+        string Path { get; set; }
+
+        Task<Response> GetResponseAsync();
+    }
+}
---
+++ LuisDelValle.Calculator/LuisDelValle.Calculator.WebApp/Services/RestClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LuisDelValle.Calculator.Abstractions;
+using LuisDelValle.Calculator.CalculatorService.Models;
+using Newtonsoft.Json;
+
+namespace LuisDelValle.Calculator.WebApp.Services
+{
+    public class RestClient : IRestClient
+    {
+        public string Host { get; set; }
+        public string Path { get; set; }
+
+        private static HttpClient Client = new HttpClient();
+
+        public async Task<Response> GetResponseAsync()
+        {
+            var stringResponse = await Client.GetStringAsync(Host + Path);
+
+            if (stringResponse != null)
+            {
+                SequencesResponse sequenceResponse = JsonConvert.DeserializeObject<SequencesResponse>(stringResponse);
+
+                if (sequenceResponse != null)
+                {
+                    return sequenceResponse;
+                }
+            }
+
+            return null;
+        }
+    }
+}
