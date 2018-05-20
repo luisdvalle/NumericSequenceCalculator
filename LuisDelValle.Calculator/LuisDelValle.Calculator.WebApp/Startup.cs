e5c16f88d481bed0f99d76bbc4214576612eb433
luisdvalle <jums.delvalle@gmail.com>
2018-05-20 19:03:19 +1000

    Initial setup for a home page. Adding controller, views and Startup configuration

--- LuisDelValle.Calculator/LuisDelValle.Calculator.WebApp/Startup.cs
+++ LuisDelValle.Calculator/LuisDelValle.Calculator.WebApp/Startup.cs
@@ -15,6 +15,7 @@ namespace LuisDelValle.Calculator.WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,10 @@ namespace LuisDelValle.Calculator.WebApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // Adding middleware to the pipeline
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
         }
     }
 }
